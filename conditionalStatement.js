d6f182c9bc1c60c81502013d6a1e81ac5ee6db82
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-19 20:11:29 +0530

    uploaded !

---
+++ conditionalStatement.js
@@ -0,0 +1,39 @@
+// if_Statement
+
+// var d= new Date();
+// var time =d.getHours();
+
+// if(time<10){
+//     console.log("Good Morning!!");
+// }
+
+// var age = 18;
+
+// if(age<=18){
+//     console.log("You are an adult..")
+// }
+
+
+// if_else statements
+// var Age=18;
+
+// if(Age<=18){
+//     console.log("You are an adult.")
+// }
+// else{
+//     console.log("You are a child.")
+// }
+
+// if-else if-else Statement
+
+var d = new Date()
+var time = d.getHours()
+if (time<10){
+    console.log("Good morning ");
+}
+else if(time>=10 && time<16){
+    console.log("Good day ");
+}
+else{
+    console.log("Hello world ")
+}
