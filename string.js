4e875e37c2b7b55e60dcfc9d72c3b7b47fc4d38c
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-20 18:42:28 +0530

    uploaded !

---
+++ string.js
@@ -0,0 +1,24 @@
+let mn= "Levi"
+console.log (mn . length)//for length of string
+console.log (mn[0])//for the character of string 
+
+let nm =  'Tsunade'
+console.log (nm)
+
+
+
+//Templet litres
+let boy1= "Ronald"
+let boy2= "Richard"
+
+let combi= `${boy2} is a friend of ${boy1}`
+console.log (combi)
+//we can insert variable directly in Templet literal...is known as string interpolation 
+
+
+//Escape Sequence Character 
+
+let fruit= 'bana\'na'
+console.log (fruit)
+
+
