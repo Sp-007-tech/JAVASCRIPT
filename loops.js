2a3e67fb6caad969f5876d57a62354141f8b5e7b
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-20 18:44:37 +0530

    uploaded !

---
+++ loops.js
@@ -0,0 +1,35 @@
+// for loop
+
+// var i = 0;
+// for(i=0;i<=10;i++){
+//     console.log("The number is ",i)
+// }
+
+// var a;
+// for(a=0;a<=10;a++){
+//     console.log("A",(a),"B",(a+10));
+// }
+
+
+// while loop
+
+// var a=0;
+
+// while(a<10){
+//     console.log("The number is ",a)
+//     a++
+// }
+
+// do-while
+
+// var a=0;
+
+// do{
+//     console.log("the number is ",a);
+//     a++;
+// }
+// while(a<7);
+
+
+
+
