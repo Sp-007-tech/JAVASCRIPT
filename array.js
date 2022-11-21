640e50293abb8a4069b48d56931c57116084555c
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2022-11-21 16:49:19 +0530

    uploaded !

---
+++ array.js
@@ -0,0 +1,34 @@
+/*(1)Arrays are the some collection of items ....means that we can add some Boolean,some number ,we can add we can access them from their index ..
+(2)Arrays are mutable....*/
+
+
+
+
+let marks= [81,29,53,49,75,79,null,Boolean]
+console.log(marks)
+marks[8]= 99//to add new value in array
+marks[4] = 36 //changing the value of an Array 
+console.log(marks[0])
+console.log(marks[1])
+console.log(marks[2])
+console.log(marks[3])
+console.log(marks[4])
+console.log(marks[5])
+console.log(marks[6])
+console.log(marks[7])
+console.log(marks[8])
+console.log("the length is ",marks.length)
+console.log(typeof marks)*
+
+
+//quiz
+for(let i in marks){
+console.log(marks)
+
+}
+
+
+
+
+
+
