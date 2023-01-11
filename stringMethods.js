94c5c16aa5503bf10cc54d25b630258db36426eb
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2023-01-11 21:58:39 +0530

    Updated !

--- stringMethods.js
+++ stringMethods.js
@@ -1,8 +1,5 @@
 //String are immutable ....every method return a new string but the original string can't be changed.. 
 
-
-
-
 let nm = "shailesh"
 /*console.log (nm.length)
 console.log (nm.toUpperCase())
@@ -12,7 +9,6 @@ console.log (nm.replace("shailesh","Elon"))*/
 
 let frnd = "Naman"
 console.log (nm.concat(" is a friend of ", frnd , " Ok "))
-
 
 
 //using for loop we can print string
